ad358a20ed2a8bdcf254272fbde5e276f6b1077c
Michael Grubb <michael@grubb.co>
2015-01-28 11:16:24 -0600

    Update dependency versions

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/java.classpath "0.2.2"]
-                 [org.clojure/tools.namespace "0.2.7"]])
+                 [org.clojure/tools.namespace "0.2.8"]])
