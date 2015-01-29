0835adc7309473f0ca6dd918d9f49338b83408e6
Michael Grubb <michael@grubb.co>
2015-01-29 11:38:58 -0600

    Moved version number to snapshot for testing

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject co.grubb/uri-handler "0.1.0"
+(defproject co.grubb/uri-handler "0.2.0-SNAPSHOT"
   :description "A Library which will search the classpath to load URI handlers"
   :url "http://github.com/mgrubb/uri-handler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/java.classpath "0.2.2"]
-                 [org.clojure/tools.namespace "0.2.8"]])
+                 [org.clojure/tools.namespace "0.2.8"]]
+  :profile {:dev
+            {:dependencies [[midje "1.6.3"]]}})
