5a7d3ee65831484ce34dbcf51e4e2c1029b45144
Michael Grubb <michael@grubb.co>
2015-01-29 23:21:58 -0600

    Fix profile to profiles

--- project.clj
+++ project.clj
@@ -6,5 +6,4 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/java.classpath "0.2.2"]
                  [org.clojure/tools.namespace "0.2.8"]]
-  :profile {:dev
-            {:dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
