496e189857e6e09a56e85f74c52c4047315a5c65
Michael Grubb <michael@grubb.co>
2015-02-02 10:18:27 -0600

    Add documentation for co.grubb.uri

--- src/co/grubb/uri.clj
+++ src/co/grubb/uri.clj
@@ -1,15 +1,21 @@
+;; ## URI manipulation and Utilities
 (ns co.grubb.uri
   (:require [clojure.string :as str])
   (:import [java.net URI]))
 
 (defn- split-user-info
+  "Takes a raw user-info URI field and splits it to a map.
+  The map contains the keys :username and :password. The username
+  and password from the user-info may be separated with a `:` or `;`."
   [s]
   (when (seq s)
     (into {} (mapcat hash-map
                      [:username :password]
                      (str/split s #":|;" 2)))))
-
 (defn map->URI
+  "Takes a map, `m`, that contains URI fields and returns a `java.net.URI` object.
+  The map supports the keys: `:scheme`, `:user-info`, `:host`, `:port`, `:path`,
+  `:query`, and `:fragment`."
   [m]
   (URI. (:scheme m)
         (:user-info m)
@@ -21,17 +27,18 @@
 
 (defprotocol URILike
   "A protocol for dealing with things that act like URIs."
-  (uri [x])
-  (scheme [uri])
-  (host [uri])
-  (port [uri])
-  (path [uri])
-  (user-info [uri])
-  (user-info-map [uri])
-  (query [uri])
-  (fragment [uri])
-  (authority [uri])
-  (scheme-part [uri]))
+  (uri [x] "Function to convert `x` to a `java.net.URI` object.")
+  (scheme [uri] "Return the scheme part of the URI.")
+  (host [uri] "Return the host part of the URI.")
+  (port [uri] "Return the port part of the URI.")
+  (path [uri] "Return the path part of the URI, sans the query and the fragment")
+  (user-info [uri] "Return the unprocessed user information part of the URI.")
+  (user-info-map [uri] "Return a map of `:username` and `:password` parsed from user-info string.
+                       Username and Password are separated either by `:` or `;`.")
+  (query [uri] "Return the query portion of the URI.")
+  (fragment [uri] "Return the fragment portion of the URI.")
+  (authority [uri] "Returns the URI's authority part (typically host and port for heirarchichal URIs.")
+  (scheme-part [uri] "Returns the scheme specific portion (i.e. everything after the scheme) of the URI."))
 
 (extend-protocol URILike
   java.net.URI
