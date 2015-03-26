3f976bb065f1c8cd12db7c304f084e0c6f561bfc
Michael Grubb <michael@grubb.co>
2015-03-26 15:29:03 -0500

    Add lein-set-version to dev workflow

--- project.clj
+++ project.clj
@@ -7,11 +7,13 @@
                  [org.clojure/java.classpath "0.2.2"]
                  [org.clojure/tools.namespace "0.2.8"]
                  [co.grubb/clj-utils "0.3.1"]]
+  :plugins [[lein-set-version "LATEST"]]
   :aliases {"lint" ["do"
                     ["bikeshed" "-m" "132" "-v"]
                     ["kibit"]
                     ["eastwood" "{:namespaces [:source-paths]}"]]}
   ;; Commented out for now due to bug with technomancy/leiningen #1821
   ; :pom-location "target/"
+  :set-version {:updates [{:path "README.md" :no-snapshot true}]}
   :deploy-branches ["master"]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
